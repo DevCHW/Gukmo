42632df0a8cf5c57399b50b0eefc32043bb3ddb0
choluckluck <chohs077@gamil.com>
2022-11-15 09:45:41 +0900

    현우씨... 받으세요

--- Gukmo/src/main/java/com/gukmo/board/hasol/controller/BoardController.java
+++ Gukmo/src/main/java/com/gukmo/board/hasol/controller/BoardController.java
@@ -2,6 +2,9 @@ package com.gukmo.board.hasol.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.gukmo.board.hasol.service.InterBoardService;
 
@@ -11,5 +14,4 @@ public class BoardController {
 	@Autowired
 	private InterBoardService service;
 	
-
 }
