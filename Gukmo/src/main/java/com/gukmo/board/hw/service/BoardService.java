4831b92987c5dc24eab8a938c5e4e62124589b4c
hyunwoocastle <112748454+hyunwoocastle@users.noreply.github.com>
2022-11-19 22:28:19 +0900

    /community/studies.do 페이징처리, 리스트 불러와서 화면 출력 완성(추후 검색어 조건까지 구현예정)

--- Gukmo/src/main/java/com/gukmo/board/hw/service/BoardService.java
+++ Gukmo/src/main/java/com/gukmo/board/hw/service/BoardService.java
@@ -2,10 +2,12 @@ package com.gukmo.board.hw.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.gukmo.board.hw.controller.BoardVO;
 import com.gukmo.board.hw.repository.BoardDAO;
 import com.gukmo.board.hw.repository.InterBoardDAO;
 import com.gukmo.board.model.MemberVO;
@@ -15,6 +17,30 @@ public class BoardService implements InterBoardService{
 	
 	@Autowired
 	private InterBoardDAO dao;
+
+	/**
+	 * study 게시판을 보여주기 위한 BoardVO 리스트 가져오기
+	 * @param paraMap(검색어,시작rownum,끝rownum)
+	 * @return BoardVO리스트
+	 */
+	@Override
+	public List<BoardVO> getStudies(Map<String, String> paraMap) {
+		List<BoardVO> studies = dao.getStudies(paraMap);
+		return studies;
+	}
+
+	
+	
+	/**
+	 * 총 게시물 건수(totalCount) 구하기
+	 * @param paraMap(검색어)
+	 * @return (총 게시물 건수) 를 반환한다. totalCount
+	 */
+	@Override
+	public int getTotalStudiesCount(Map<String, String> paraMap) {
+		int totalCount = dao.getTotalStudiesCount(paraMap);
+		return totalCount;
+	}
 
 	
 	
