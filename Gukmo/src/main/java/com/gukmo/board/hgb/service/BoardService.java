3d09d5985063186c673e6b511086895b6e9dea27
GGWANGBIN <fprhffjtm30@naver.com>
2022-11-27 18:02:16 +0900

    디테일페이지 푸시

--- Gukmo/src/main/java/com/gukmo/board/hgb/service/BoardService.java
+++ Gukmo/src/main/java/com/gukmo/board/hgb/service/BoardService.java
@@ -1,6 +1,7 @@
 package com.gukmo.board.hgb.service;
 
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -20,43 +21,28 @@ public class BoardService implements InterBoardService{
 	
 	@Autowired   // Type 에 따라 알아서 Bean 을 주입해준다.
 	private FileManager fileManager;
-		
-	// 글1개 조회하기
-	@Override
-	public BoardVO getView(Map<String, String> paraMap) {
-		
-		BoardVO boardvo = dao.getView(paraMap); // 글1개 조회하기 
-		
-		/*
-		String login_userid = paraMap.get("login_userid");
-		// paraMap.get("login_userid") 은 로그인을 한 상태이라면 로그인한 사용자의 userid 이고,
-		// 로그인을 하지 않은 상태이라면  paraMap.get("login_userid") 은 null 이다. 
-		
-		if(login_userid != null &&
-		   boardvo != null && 
-		   !login_userid.equals(boardvo.getNickname()) ) {
-			// 글조회수 증가는 로그인을 한 상태에서 다른 사람의 글을 읽을때만 증가하도록 한다.
-			
-			dao.setAddReadCount(boardvo.getBoard_num()); // 글조회수 1증가 하기 
-			boardvo = dao.getView(paraMap);
-		}
-		*/
-		
-		return boardvo;
-		
-	}
 
-	// 글조회수 증가는 없고 단순히 글1개만 조회 해주는 것
-	@Override
-	public BoardVO getViewWithNoAddCount(Map<String, String> paraMap) {
-		BoardVO boardvo = dao.getView(paraMap); // 글1개 조회하기
-		return boardvo;
-	}
 	
 	
-
-
-
+	/**
+	 * 하나의 글 불러오기
+	 * @param 글번호 board_num
+	 * @return BoardVO
+	 */
+	@Override
+	public BoardVO getBoardDetail(int board_num) {
+		
+		//상세카테고리 알아오기
+		String detail_category = dao.getCategory(board_num);
+		Map<String,String> paraMap = new HashMap<>();
+		
+		paraMap.put("board_num", board_num+"");
+		paraMap.put("detail_category",detail_category);
+		
+		BoardVO board = dao.getBoardDetail(paraMap);
+		return board;
+	}
+	
 
 
 	
