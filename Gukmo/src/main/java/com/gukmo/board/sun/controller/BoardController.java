d6c9f0f8c39b20cd095cfc7b3890e3982932ca69
choluckluck <chohs077@gamil.com>
2022-12-20 00:23:43 +0900

    안녕~

--- Gukmo/src/main/java/com/gukmo/board/sun/controller/BoardController.java
+++ Gukmo/src/main/java/com/gukmo/board/sun/controller/BoardController.java
@@ -717,7 +717,6 @@ public class BoardController {
 	      Map<String,String> alarmMap = new HashMap<>();
 	      alarmMap.put("alarm_nickname", paraMap.get("reported_nickname"));
 	      //System.out.println("url_num:" + paraMap.get("fk_num"));
-	      
     	  alarmMap.put("cmd", "penalty");
     	  alarmMap.put("url", "/detail.do?boardNum=");
     	  alarmMap.put("content", paraMap.get("subject"));
